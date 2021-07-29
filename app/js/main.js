d4a5cbcdecbd171af0df181a360363105ef7c222
ivansheshin <ivansheshin@gmail.com>
2021-07-29 22:38:16 +0300

    анимация пунктов фильтра

--- app/js/main.js
+++ app/js/main.js
@@ -1,23 +1,19 @@
 const assortment = document.querySelectorAll('.assortment__item')
-
-
 // Константы для фильтра
 const filter = document.querySelector('.types')
-const filterAllItem = document.querySelectorAll('types__item')
+const filterAllItem = document.querySelectorAll('.types__item')
 // 
 // Слушатели на фильтры
-
+console.log(filterAllItem)
 filter.addEventListener('click', e => {
-    if(e.target.tagName !== 'LI') return 
+    if (e.target.tagName !== 'LI') return
 
     const filterId = e.target.id;
     const filterItem = e.target
 
-    if(!filterItem.classList.contains('clicked')){
+    if (!filterItem.classList.contains('clicked')) {
+        filterAllItem.forEach(item => item.classList.remove('clicked'))
         filterItem.classList.add('clicked')
-    } else{
-        filterItem.classList.remove('clicked')
-
     }
 
     assortment.forEach(item => {
