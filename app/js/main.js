1b514ad8f3c46c951a0ecbb402108d8b6829eb4e
ivansheshin <ivansheshin@gmail.com>
2021-07-29 22:26:14 +0300

    123

--- app/js/main.js
+++ app/js/main.js
@@ -1,37 +1,31 @@
 const assortment = document.querySelectorAll('.assortment__item')
-// Константы для типов пицц
-const ClosedPizza = document.querySelectorAll('.closed')
-const SpicyPizza = document.querySelectorAll('.spicy')
-const GrillPizza = document.querySelectorAll('.grill')
-const VegetarianPizza = document.querySelectorAll('.vegetarian')
-const MeatPizza = document.querySelectorAll('.meat')
+
 
 // Константы для фильтра
 const filter = document.querySelector('.types')
-const filterItem = document.querySelectorAll('types__item')
-const filterAll = document.querySelector('#all');
-const filterMeat = document.querySelector('#meat');
-const filterVegetarian = document.querySelector('#vegetarian');
-const filterGrill = document.querySelector('#grill');
-const filterSpicy = document.querySelector('#spicy');
-const filterClosed = document.querySelector('#closed');
+const filterAllItem = document.querySelectorAll('types__item')
 // 
-let arrayFiltres = [filterAll, filterMeat, filterVegetarian]
 // Слушатели на фильтры
 
 filter.addEventListener('click', e => {
-    let filterItem = e.target.id;
+    if(e.target.tagName !== 'LI') return 
+
+    const filterId = e.target.id;
+    const filterItem = e.target
+
+    if(!filterItem.classList.contains('clicked')){
+        filterItem.classList.add('clicked')
+    } else{
+        filterItem.classList.remove('clicked')
+
+    }
 
     assortment.forEach(item => {
-        if (!item.classList.contains(filterItem) && filterItem !== 'all') {
+        if (!item.classList.contains(filterId) && filterId !== 'all') {
             item.style.display = 'none'
         } else {
             item.style.display = ''
         }
     })
-
-
-    // assortment.forEach(item=>item.style.display = 'none')
-    // pizzaType.forEach(item => item.style.display = 'block')
 
 })
