9e61a67118290bed9d8cc2bf381195f06649ebc5
ivansheshin <ivansheshin@gmail.com>
2021-07-29 19:55:28 +0300

    функция для слушателей

--- app/js/main.js
+++ app/js/main.js
@@ -1,42 +1,15 @@
-function initListeners() {
-    const categories = document.querySelectorAll('.types__item');
-
-
-//     categories.forEach(item => item.addEventListener('click', () => {
-//         const clickedStyle = document.querySelector('.clicked')
-//         // if(clickedStyle === 0 || !clickedStyle)
-//         console.log(!clickedStyle)
-//         item.classList.toggle('clicked')
-//         item.classList.toggle('types__item_hover')
-        
-//         if (clickedStyle){
-//             item.classList.remove('clicked');
-            
-//         }
-//         // if(!clickedStyle){
-            
-//         //     item.classList.toggle('clicked')
-//         //     item.classList.toggle('types__item_hover')
-//         // } else{
-//         //     categories.forEach(item => item.classList.remove('clicked'))
-//         //     item.classList.toggle('clicked')
-//         //     item.classList.toggle('types__item_hover')
-//         // }
-        
-//     }))
-
-}
 
 
 const assortment = document.querySelectorAll('.assortment__item')
 // Константы для типов пицц
-const selectorsClosedPizza = document.querySelectorAll('.closed')
-const selectorsSpicyPizza = document.querySelectorAll('.spicy')
-const selectorsGrillPizza = document.querySelectorAll('.grill')
-const selectorsVegetarianPizza = document.querySelectorAll('.vegetarian')
-const selectorsMeatPizza = document.querySelectorAll('.meat')
+const ClosedPizza = document.querySelectorAll('.closed')
+const SpicyPizza = document.querySelectorAll('.spicy')
+const GrillPizza = document.querySelectorAll('.grill')
+const VegetarianPizza = document.querySelectorAll('.vegetarian')
+const MeatPizza = document.querySelectorAll('.meat')
 
-// Константы для кнопок фильтра
+// Константы для фильтра
+const filterItem = document.querySelectorAll('types__item')
 const filterAll = document.querySelector('#all');
 const filterMeat = document.querySelector('#meat');
 const filterVegetarian = document.querySelector('#vegetarian');
@@ -44,34 +17,26 @@ const filterGrill = document.querySelector('#grill');
 const filterSpicy = document.querySelector('#spicy');
 const filterClosed = document.querySelector('#closed');
 // 
+let arrayFiltres = [filterAll, filterMeat, filterVegetarian]
 // Слушатели на фильтры
+
+
 filterAll.addEventListener('click', ()=>{
+    filterAll.classList.add('clicked')
     assortment.forEach(item=>item.style.display = 'block')
 })
-filterMeat.addEventListener('click', ()=>{
-    assortment.forEach(item=>item.style.display = 'none')
-    selectorsMeatPizza.forEach(item => item.style.display = 'block')
-})
-filterVegetarian.addEventListener('click', ()=>{
-    assortment.forEach(item=>item.style.display = 'none')
-    selectorsVegetarianPizza.forEach(item => item.style.display = 'block')
-})
-filterGrill.addEventListener('click', ()=>{
-    assortment.forEach(item=>item.style.display = 'none')
-    selectorsGrillPizza.forEach(item => item.style.display = 'block')
-})
-filterSpicy.addEventListener('click', ()=>{
-    assortment.forEach(item=>item.style.display = 'none')
-    selectorsSpicyPizza.forEach(item => item.style.display = 'block')
-})
-filterClosed.addEventListener('click', ()=>{
-    assortment.forEach(item=>item.style.display = 'none')
-    selectorsClosedPizza.forEach(item => item.style.display = 'block')
-})
 
-// 
+function initListenersForFiltres(element, assortment, pizzaType ){
+    element.addEventListener('click', ()=>{
+        element.classList.add('clicked')
+        assortment.forEach(item=>item.style.display = 'none')
+        pizzaType.forEach(item => item.style.display = 'block')
+    })
+}
 
+initListenersForFiltres(filterMeat,assortment,MeatPizza)
+initListenersForFiltres(filterVegetarian,assortment,VegetarianPizza)
+initListenersForFiltres(filterGrill,assortment,GrillPizza)
+initListenersForFiltres(filterSpicy,assortment,SpicyPizza)
+initListenersForFiltres(filterClosed,assortment,ClosedPizza)
 
-
-
-initListeners();
