8eb33c569f3b63dba5ade623787d3e29f30e5c7a
ivansheshin <ivansheshin@gmail.com>
2021-09-18 10:47:34 +0300

    перенос фильтра по категориям в модуль фильтрации

--- app/js/main.js
+++ app/js/main.js
@@ -1,47 +1,17 @@
 import {PizzaAssortment} from './PizzaAssortment';
 import {renderGoodsCards} from "./HTMLCreateGoods";
 import {sortByNamePricePopular} from "./sortByNamePricePopular";
-import { popupForSort } from './sortByNamePricePopular';
+// import { popupForSort } from './sortByNamePricePopular';
 import '../scss/style.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
-    popupForSort();
+    // popupForSort();
     renderGoodsCards(PizzaAssortment)
     const btnFilterChooseAll = document.querySelector('#btn-filter-choose-all')
     btnFilterChooseAll.classList.add('types__item_clicked');
 
-    const filterBlock = document.querySelector('.types')
-    const filterBlockItems = document.querySelectorAll('.types__item')
-    
-    let filteredArray = [];
 
-    filterBlock.addEventListener('click', e => {
-        const filterItem = e.target
-
-        if (filterItem.tagName !== 'LI') return
-        // Анимация при выборе категории
-        if (!filterItem.classList.contains('types__item_clicked')) {
-            filterBlockItems.forEach(item => item.classList.remove('types__item_clicked'))
-            filterItem.classList.add('types__item_clicked')
-        }
-
-        // Фильтрация
-        const allGoods = document.querySelectorAll('.assortment__item');
-
-        if (filterItem.id === 'btn-filter-choose-all') {
-            allGoods.forEach(item => item.remove())
-            renderGoodsCards(PizzaAssortment)
-        } else {
-
-            allGoods.forEach(item => item.remove())
-            filteredArray = PizzaAssortment.filter(item => item.filterKey === filterItem.textContent.toLowerCase())  
-            renderGoodsCards(filteredArray)
-            sortByNamePricePopular(filteredArray)
-        }
-        
-    })
-    sortByNamePricePopular(PizzaAssortment)
-
+    sortByNamePricePopular()
     // 
     const pizzaType = document.querySelectorAll('.pizza-information__item');
     
--- app/js/sortByNamePricePopular.js
+++ app/js/sortByNamePricePopular.js
@@ -1,49 +1,82 @@
-import { renderGoodsCards } from "./HTMLCreateGoods"
-export function sortByNamePricePopular(array) {
+import {
+    renderGoodsCards
+} from "./HTMLCreateGoods";
+import {PizzaAssortment} from './PizzaAssortment';
+export function sortByNamePricePopular() {
 
-    // Изменение текста кнопки и фильтрация по названию\цену\популярности
-    const sortItem = document.querySelectorAll('.sort__item')
-    const sortButton = document.querySelector('.sort__choosen')
+    //     // Изменение текста кнопки и фильтрация по названию\цену\популярности
+    //     const sortItem = document.querySelectorAll('.sort__item')
+    //     const sortButton = document.querySelector('.sort__choosen')
 
-    sortItem.forEach(item => item.addEventListener('click', e => {
-        const filterItem = e.target;
-        sortButton.textContent = filterItem.textContent;
+    //     sortItem.forEach(item => item.addEventListener('click', e => {
+    //         const target = e.target;
+    //         sortButton.textContent = target.textContent;
 
+    //         const allGoods = document.querySelectorAll('.assortment__item');
+    //         if(target.id === 'name'){
+    //             allGoods.forEach(item => item.remove())
+    //             sortArrayByName(array)
+
+    //             renderGoodsCards(array)
+
+    //         }
+    //         else if(target.id === 'price'){
+    //             allGoods.forEach(item => item.remove())
+    //             sortArrayByPrice(array)
+    //             renderGoodsCards(array)
+    //         }
+    //     }))
+
+    // }
+
+    // export function popupForSort(){
+    //     const sortBlock = document.querySelector('.sort');
+    //     const sortList = document.querySelector('.sort__list');
+    //     const sortArrow = document.querySelector('.sort__svg');
+
+    //     sortBlock.addEventListener('click', () => {
+    //         sortList.classList.toggle('open')
+
+    //     })
+    // }
+    // function sortArrayByPrice(array){
+    //     array.sort((frstItem, scndItem) => frstItem.price - scndItem.price)
+    // }
+
+    // function sortArrayByName(array){
+    //     array.sort((frstItem, scndItem) => {
+    //         if(frstItem.title > scndItem.title) return 1;
+    //         if(frstItem.title === scndItem.title) return 0;
+    //         if(frstItem.title < scndItem.title) return -1;
+    //     })
+    // }
+    const filterBlock = document.querySelector('.types')
+    const filterBlockItems = document.querySelectorAll('.types__item')
+    
+    let filteredArray = [];
+    
+    filterBlock.addEventListener('click', ({target}) => {
+
+        if (target.tagName !== 'LI') return
+        // Анимация при выборе категории
+        if (!target.classList.contains('types__item_clicked')) {
+            filterBlockItems.forEach(item => item.classList.remove('types__item_clicked'))
+            target.classList.add('types__item_clicked')
+        }
+
+        // Фильтрация по категориям
         const allGoods = document.querySelectorAll('.assortment__item');
-        if(filterItem.id === 'name'){
+
+        if (target.id === 'btn-filter-choose-all') {
             allGoods.forEach(item => item.remove())
-            sortArrayByName(array)
-            
-            renderGoodsCards(array)
+            renderGoodsCards(PizzaAssortment)
+        } else {
 
-        }
-        else if(filterItem.id === 'price'){
             allGoods.forEach(item => item.remove())
-            sortArrayByPrice(array)
-            renderGoodsCards(array)
+            filteredArray = PizzaAssortment.filter(item => item.filterKey === target.textContent.toLowerCase())
+            renderGoodsCards(filteredArray)
+            sortByNamePricePopular(filteredArray)
         }
-    }))
 
-}
-
-export function popupForSort(){
-    const sortBlock = document.querySelector('.sort');
-    const sortList = document.querySelector('.sort__list');
-    const sortArrow = document.querySelector('.sort__svg');
-
-    sortBlock.addEventListener('click', () => {
-        sortList.classList.toggle('open')
-  
-    })
-}
-function sortArrayByPrice(array){
-    array.sort((frstItem, scndItem) => frstItem.price - scndItem.price)
-}
-
-function sortArrayByName(array){
-    array.sort((frstItem, scndItem) => {
-        if(frstItem.title > scndItem.title) return 1;
-        if(frstItem.title === scndItem.title) return 0;
-        if(frstItem.title < scndItem.title) return -1;
     })
 }
