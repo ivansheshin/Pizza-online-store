51381e1fba5a68608ce44d6c88beb951976cf271
ivansheshin <ivashesh@gmail.com>
2022-06-23 20:02:29 +0300

    open indexedDB

--- app/js/main.js
+++ app/js/main.js
@@ -10,6 +10,19 @@ import SelectSize from 'Script/SelectSize';
 import Basket from 'Script/Basket';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const request = indexedDB.open('PizzaStore');
+
+  const db = request.result;
+  const store = db.createObjectStore('books', { keyPath: 'isbn' });
+  const titleIndex = store.createIndex('by_title', 'title', { unique: true });
+  const authorIndex = store.createIndex('by_author', 'author');
+
+  // Populate with initial data.
+  store.put({ title: 'Quarry Memories', author: 'Fred', isbn: 123456 });
+  store.put({ title: 'Water Buffaloes', author: 'Fred', isbn: 234567 });
+  store.put({ title: 'Bedrock Nights', author: 'Barney', isbn: 345678 });
+
+
   const assortmentContainer = document.querySelector('.assortment__list');
 
   const renderGoods = new RenderGoodsCards(assortment, assortmentContainer);
