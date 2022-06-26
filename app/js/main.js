872289680000c81fb10bf66e847c022ed4ee2f79
ivansheshin <ivashesh@gmail.com>
2022-06-26 12:26:54 +0300

    feat: created init indexedb

--- app/js/main.js
+++ app/js/main.js
@@ -1,62 +1,31 @@
-/* eslint-disable no-new */
-/* eslint-disable import/no-unresolved */
-import 'Style/style.scss';
-import assortment from 'Data/assortment.json';
-import RenderGoodsCards from 'Script/RenderGoodsCards';
-import Filter from 'Script/Filters';
-import Sort from 'Script/Sort';
-import SelectParameters from 'Script/SelectParameters';
-import SelectSize from 'Script/SelectSize';
-import Basket from 'Script/Basket';
-
-document.addEventListener('DOMContentLoaded', () => {
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable func-names */
+export function createPizzaStore(assortment) {
   const request = indexedDB.open('PizzaStore');
   let db;
 
-  request.onupgradeneeded = function() {
+  request.onupgradeneeded = function () {
     const dataBase = request.result;
-    const store = dataBase.createObjectStore('assortment', { keyPath: 'assortmentKey' });
-    // const titleIndex = store.createIndex('by_title', 'title', { unique: true });
-    // const authorIndex = store.createIndex('by_author', 'author');
+    dataBase.createObjectStore('assortment', {
+      keyPath: 'assortmentKey',
+    });
+  };
 
+  request.onsuccess = function () {
+    db = request.result;
+    const transaction = db.transaction('assortment', 'readwrite');
+    const assortmentStore = transaction.objectStore('assortment');
     assortment.forEach((assortmentItem) => {
-      store.put({
+      assortmentStore.add({
         title: assortmentItem.title,
-        // imgAddress: assortmentItem.imgAddress,
-        // altImg: assortment.altImg,
-        // dough: assortment.dough,
-        // sizes: assortment.sizes,
-        // price: assortment.price,
-        // filterKey: assortment.filterKey,
-        assortmentKey: 1,
+        imgSrc: assortmentItem.imgSrc,
+        altImg: assortmentItem.altImg,
+        dough: assortmentItem.dough,
+        sizes: assortmentItem.sizes,
+        price: assortmentItem.price,
+        filterKey: assortmentItem.filterKey,
+        assortmentKey: assortmentItem.id,
       });
     });
-
-    // Populate with initial data.
-    // store.put({ title: 'Quarry Memories', author: 'Fred', assortmentKey: 123456 });
-    // store.put({ title: 'Water Buffaloes', author: 'Fred', assortmentKey: 234567 });
-    // store.put({ title: 'Bedrock Nights', author: 'Barney', assortmentKey: 345678 });
   };
-
-  request.onsuccess = function() {
-    db = request.result;
-  };
-
-  const assortmentContainer = document.querySelector('.assortment__list');
-
-  const renderGoods = new RenderGoodsCards(assortment, assortmentContainer);
-  renderGoods.render();
-
-  const filterCategories = document.querySelectorAll('.filter-categories__item');
-  new Filter(assortmentContainer, filterCategories);
-
-  const sortContainer = document.querySelector('.sort');
-  new Sort(assortmentContainer, sortContainer);
-
-  const pizzaInformationContainers = document.querySelectorAll('.pizza-information');
-  new SelectParameters(pizzaInformationContainers);
-
-  new SelectSize(assortment);
-
-  new Basket();
-});
+}
