59498835948b4b4f3de3738fddda69c6c7d86a8e
i.sheshin <i.sheshin@tradesoft.ru>
2022-06-28 17:35:54 +0300

    reworked, added amount of goods update into basket

--- app/js/Basket.js
+++ app/js/Basket.js
@@ -2,7 +2,8 @@ const defaultParams = {
   assortmentItemSelector: '.assortment__item',
   assortmentButtonSelector: '.assortment__btn',
   assortmentActiveButtonSelector: 'assortment__btn_active',
-  amountGoodsSelector: 'order__quantity',
+  amountSelector: '.order__amount',
+  sumBasketSelector: '.order__price',
 };
 
 export default class Basket {
@@ -19,9 +20,11 @@ export default class Basket {
     const {
       assortmentItemSelector,
       assortmentButtonSelector,
+      amountSelector,
     } = this.params;
 
     this.addedPizza = [];
+    this.amountGoodsItem = document.querySelector(amountSelector);
 
     const assortmentItems = document.querySelectorAll(assortmentItemSelector);
 
@@ -36,27 +39,23 @@ export default class Basket {
 
     if (!isButton) return;
 
-    const { assortmentActiveButtonSelector, amountGoods } = this.params;
+    this.switchAddBtnState(assortmentButton, assortmentItem);
+    this.setInfoToLocalStorage();
+    this.updateInfoBasket();
+  }
+
+  switchAddBtnState(assortmentButton, assortmentItem) {
+    const { assortmentActiveButtonSelector } = this.params;
 
     assortmentButton.classList.toggle(assortmentActiveButtonSelector);
     const isAdded = assortmentButton.classList.contains(assortmentActiveButtonSelector);
 
-    Basket.changeButton(assortmentButton, isAdded);
-    if (isAdded) {
-      this.addToBasket(assortmentItem);
-    } else {
-      this.removeFromBasket(assortmentItem);
-    }
-
-    const idCollection = this.addedPizza.join(',');
-    localStorage.setItem('PizzaId', idCollection);
-  }
-
-  static changeButton(assortmentButton, isAdded) {
     if (isAdded) {
       assortmentButton.innerText = 'Удалить';
+      this.addToBasket(assortmentItem);
     } else {
       assortmentButton.innerText = '+Добавить';
+      this.removeFromBasket(assortmentItem);
     }
   }
 
@@ -67,5 +66,15 @@ export default class Basket {
   removeFromBasket(assortmentItem) {
     const index = this.addedPizza.findIndex((item) => item === assortmentItem.id);
     this.addedPizza.splice(index, 1);
+  }
+
+  setInfoToLocalStorage() {
+    const idCollection = this.addedPizza.join(',');
+    localStorage.setItem('PizzaId', idCollection);
+  }
+
+  updateInfoBasket() {
+    const amountAddedPizza = this.addedPizza.length;
+    this.amountGoodsItem.textContent = amountAddedPizza;
   }
 }
