0eaf5caec75ac93ae329a9159c16f89c5f9a1636
i.tuev <sadiolem.sds@gmail.com>
2022-08-26 17:21:08 +0300

    feedback comments for home page html

--- app/js/RenderGoodsCards.js
+++ app/js/RenderGoodsCards.js
@@ -10,7 +10,12 @@ export default class RenderGoodsCards {
       this.container.insertAdjacentHTML('beforeend', `
         <div class="assortment__item" id="${item.id}" data-category="${item.filterKey}">
           <img src="${item.imgSrc}" width="260" alt="${item.altImg}">
+          <!-- для этих картинок важно указывать высоту через атрибут height -->
+          <!-- иначе при медленной загрузке сильно дергается контент -->
+          <!-- можешь локально в network 'no throttling' поменять хотя бы на fast 3g, увидишь разницу -->
           <h2 class="assortment__pizza-name">${item.title}</h2>
+          <!-- выше по хтмл есть h2 заголовок 'Все пиццы', вряд ли они равнозначны, значит здесь -->
+          <!-- должен быть максимум h3 -->
           <div class="pizza-information">
             <div class="pizza-information__type">
                   ${item.dough.map((typeItem) => `<span class="pizza-information__type-item">${typeItem}</span>`).join('')}
@@ -18,6 +23,7 @@ export default class RenderGoodsCards {
             <div class="pizza-information__size">
                    ${item.sizes.map((sizeItem) => `<span class="pizza-information__size-item">${sizeItem}</span>`).join('')}
             </div>
+            <!-- если уж делаешь доступность с клавы, то почему здесь нельзя выбрать type & size? -->
           </div>
           <div class="assortment__price-to-add">
             <span class="assortment__price">от ${Object.values(item.price)[0]}</span>
